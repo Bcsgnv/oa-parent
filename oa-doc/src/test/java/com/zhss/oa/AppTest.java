3691c7492b227152147690896fd64be2469fadf0
Bcsgnv <huanshalu43390@126.com>
2021-04-06 21:19:48 +0800

    项目初始化

---
+++ oa-doc/src/test/java/com/zhss/oa/AppTest.java
@@ -0,0 +1,20 @@
+package com.zhss.oa;
+ 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
